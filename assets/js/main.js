f3472a0ff360133ba81f1af1874fb0713935e5bf
DianaQuispe <danaalienmas@gmail.com>
2017-07-25 12:27:29 -0500

    asdafsd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
-function Persona(nombre, apellido, dni, asiento) {
+var todos = [];
+var asiento;
+var asientoAct;
+
+
+
+function Persona(nombre, apellido, dni) {
 	this.nombre = nombre;
 	this.apellido = apellido;
 	this.dni = dni;
-	this.reservar = function() {
-		alert("funciona");
+	this.asiento;
+	this.reservar = function() {/*
+		var nombre = document.getElementById('nombre').value;
+	var apellido =  document.getElementById("apellido").value;
+	var dni = document.getElementById("dni").value;
+	alert( "Reservado con exito!" + "\n" + "Nombre: " + nombre + "\n" +  "Apellido: " + apellido + "\n"+ "DNI: " +  dni + "\n" +  "Número de Asiento: " +  asiento )
+		todos.push (persona);*/
+
+
+
 	}
+
 }
 
-var name = document.getElementById("nombre").value;
-var lastname = document.getElementById("nombre").value;
-var dni = document.getElementById("nombre").value;
 
-var persona = new Persona(name, lastname, dni);
+
+
+
 
 var reservarB = document.getElementById('reservar');
 reservarB.onclick = function () {
-	persona.reservar();
+	var name = document.getElementById("nombre").value;
+	var lastname = document.getElementById("apellido").value;
+	var dni = document.getElementById("dni").value;
+	var persona = new Persona(name, lastname, dni);
+	todos.push (persona);
+
+	//persona.reservar();
+	console.log(todos);
 }
